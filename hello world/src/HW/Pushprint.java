2ae3b43615e71f1d017cfa6583ee1b7877cd73d6
HJ kim <ghchfh@naver.com>
2020-11-18 19:42:37 +0900

    PushPrint.java modified on Local repository

--- hello world/src/HW/Pushprint.java
+++ hello world/src/HW/Pushprint.java
@@ -5,6 +5,7 @@ public class Pushprint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sophomore");
+		System.out.println("Wankwang Univ.");
 	}
 
 }
